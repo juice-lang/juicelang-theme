4886787a8e5eaa3158ceacb28e50b048d1254623
Josef Zoller <josef@walterzollerpiano.com>
2022-08-30 23:54:46 +0200

    Add previous and next page links

--- lib/custom_filters.rb
+++ lib/custom_filters.rb
@@ -16,6 +16,32 @@ module Jekyll
             first(where(input, property, targetValue))
         end
 
+        def previous_and_next(pageHash, nav)
+            result = {'previous' => nil, 'next' => nil}
+            page = pageFromHash(pageHash)
+
+            indexPage = pageFromHash(nav, 'index_path')
+
+            if page.equal? indexPage
+                result['next'] = pageFromHash(nav['menu'].first)
+                return result
+            end
+
+            flattenedMenu = flattenMenu(nav['menu'])
+
+            for i in 0..flattenedMenu.count
+                pageI = flattenedMenu[i]
+
+                if page.equal? pageI
+                    result['previous'] = i == 0 ? indexPage : flattenedMenu[i - 1]
+                    result['next'] = flattenedMenu[i + 1] unless i == flattenedMenu.count - 1
+                    return result
+                end
+            end
+
+            result
+        end
+
         def url_without_ext(pageHash)
             page = pageFromHash(pageHash)
             url = absolute_url(page.url)
@@ -37,16 +63,29 @@ module Jekyll
             @context.registers[:site]
         end
 
-        def pageFromHash(hash)
+        def pageFromHash(hash, pathKey = 'path')
             site.pages.find { |page|
-                page.path == hash['path']
-            }
+                page.path == hash[pathKey]
+            } unless hash.nil?
         end
 
         def isActiveRecursion?(children)
             children && children.any? { |item|
                 pageFromHash(item).equal?(currentPage) || isActiveRecursion?(item['children'])
             }
+        end
+
+        def flattenMenu(menu)
+            result = []
+
+            for item in menu
+                result.push(pageFromHash(item))
+                if item['children']
+                    result.concat(flattenMenu(item['children']))
+                end
+            end
+
+            result
         end
     end
 end
