3aca05bddc1074871c5ec97471af9a86cb800e85
Josef Zoller <josef@walterzollerpiano.com>
2022-11-09 16:20:56 +0100

    Change 'Top' to page title in toc

--- lib/custom_tags.rb
+++ lib/custom_tags.rb
@@ -16,13 +16,13 @@ class ToCTag < Liquid::Tag
         toc = Kramdown::Document.new(context.registers[:page]['content'], {
             input: "GFM"
         }).to_ToC
-        build_list(toc.select { |item| item[0] == 2 }, toc.first[1])
+        build_list(toc.select { |item| item[0] == 2 }, toc.first)
     end
 
     private
 
-    def build_list(toc, topLink)
-        result = '<li class="uk-active"><a href="#' + topLink + '">Top</a></li>' + "\n"
+    def build_list(toc, top)
+        result = '<li class="uk-active"><a href="#' + top[1] + '">' + top[2][0].value + '</a></li>' + "\n"
 
         for i in 0...toc.count
             item = toc[i]
